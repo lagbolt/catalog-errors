0619a57ae9f586c63358dc5bf50d8df7dd9fd32e
Graeme Williams <carryonwilliams@gmail.com>
2021-01-04 13:20:24 -0800

    Update secrets.py so code does not throw errors

--- lib/secrets.py
+++ lib/secrets.py
@@ -1,1 +1,12 @@
-Dummy secret file
+#
+#    This is a dummy secrets file.  Values are explained below.
+#
+
+# This is used only in seriescheck.py.  If you don't have a key
+# and this remains "", checking your series data against
+# Goodreads will be skipped
+GOODREADS_API_KEY = ""
+
+# Only needed if you read data from a MySQL database.
+# If you only read from MARC files, you're all set.
+MYSQL_PASSWORD = ""
