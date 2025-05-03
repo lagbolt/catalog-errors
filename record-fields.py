9fe61943c8b02ec6965df5461bcc01b5573a9b87
lagbolt <carryonwilliams@gmail.com>
2025-05-03 19:01:30 -0400

    Add separator command line argument

--- record-fields.py
+++ record-fields.py
@@ -1,7 +1,11 @@
 #
 #    Scan a MARC file and print out particular fields from each record.
 #
-#    Usage:  python record-fields.py  -if <MARC input file> [ -of <output file> ]  -t tag-match-1 tag-match-2 ...
+#    Usage:  python record-fields.py  
+#               -if <MARC input file>
+#               [ -of <output file> ]
+#               [ -sep <record separator string> ]
+#               -t tag-match-1 tag-match-2 ...
 #
 #    The output file defaults to the console.
 #
@@ -11,7 +15,7 @@
 #    65 matches any 65X field, and 245 matches only the 245 field.
 #
 #
-#    Version:  0.2.0  2/8/25
+#    Version:  0.2.1  5/3/25
 #
 #    License:  CC BY-NC-SA 4.0, https://creativecommons.org/licenses/by-nc-sa/4.0/
 #
@@ -37,10 +41,12 @@ def create_file_context(file, mode="", encoding="utf-8"):
 parser = argparse.ArgumentParser()
 parser.add_argument("--inputfile", "-if", help="MARC file to read", required=True)
 parser.add_argument("--outputfile", "-of", help="CSV output file", required=False, default=sys.stdout)
+parser.add_argument("--separator", "-sep", help="separator between records", required=False, default=None)
 parser.add_argument("--tags", "-t", help="output fields with these tags", required=True, nargs="+")
 args = parser.parse_args()
 
 tag_matches = args.tags
+record_separator = args.separator
 
 def match_tag(tag):
     return any (
@@ -50,7 +56,7 @@ def match_tag(tag):
 with ( open(args.inputfile, 'rb') as marc_input,
         create_file_context(args.outputfile, mode="w", encoding="utf-8") as output_file):
     for i, aRecord in enumerate(pymarc.MARCReader(marc_input)):
-        printFlag = False
+        printFlag = False      # did I print any fields from this record?
         for aField in aRecord.get_fields():
             if match_tag(aField.tag):
                 printFlag = True
@@ -58,5 +64,5 @@ with ( open(args.inputfile, 'rb') as marc_input,
                     print(f"{i:2}:  {aField}", file=output_file)
                 except UnicodeEncodeError:
                     print(f"{i:2}:  {aField.tag}  ENCODING ERROR", file=output_file)
-        if printFlag:
-            print(10*"  -", file=output_file)
+        if printFlag and record_separator:
+            print(record_separator, file=output_file)
